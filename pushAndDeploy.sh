b58636f547959919b6555e4ccb355cad2825a302
wwang33 <wwang33@ncsu.edu>
2021-07-20 22:37:25 -0700

    updated

--- pushAndDeploy.sh
+++ pushAndDeploy.sh
@@ -1,4 +1,6 @@
 git add .
 git commit -m "updated"
 git push -f origin master
-./Applications/XAMPP/xamppfiles/htdocs/mypage/deploy.sh 
+pushd /Applications/XAMPP/xamppfiles/htdocs/mypage
+./deploy.sh
+popd 
